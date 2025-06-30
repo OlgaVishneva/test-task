fd34474f69a7d5c0e3a36b3208b494a76810f8c2
Olga <olga1111vishneva.v@mail.ru>
2025-06-30 19:24:58 +0300

    Вносит некоторые правки в размер картинки и отступы

--- src/scripts/openModal.js
+++ src/scripts/openModal.js
@@ -1,44 +1,44 @@
 export function openModal() {
-    const сloseBtn = document.querySelector('.modal__close-btn');
-    const backToCatalogButton = document.querySelector('.modal__btn');
-    const modal = document.querySelector('[data-action-modal]');
-    const layout = document.querySelector('.layout');
+    // const сloseBtn = document.querySelector('.modal__close-btn');
+    // const backToCatalogButton = document.querySelector('.modal__btn');
+    // const modal = document.querySelector('[data-action-modal]');
+    // const layout = document.querySelector('.layout');
 
-    function closeModalWindow() {
-        modal.classList.remove('_active');
-        layout.classList.remove('_lock-scroll');
-        document.removeEventListener('keydown', trapFocus);
-    }
+    // function closeModalWindow() {
+    //     modal.classList.remove('_active');
+    //     layout.classList.remove('_lock-scroll');
+    //     document.removeEventListener('keydown', trapFocus);
+    // }
 
-    function openModalWindow() {
-        modal.classList.add('_active');
-        layout.classList.add('_lock-scroll');
-    }
+    // function openModalWindow() {
+    //     modal.classList.add('_active');
+    //     layout.classList.add('_lock-scroll');
+    // }
 
-    document.addEventListener('click', (event) => {
-        if (event.target.closest('[data-cross-button]')) {
-            if (!modal.classList.contains('_active')) {
-                openModalWindow();
-            }
-        }
+    // document.addEventListener('click', (event) => {
+    //     if (event.target.closest('[data-cross-button]')) {
+    //         if (!modal.classList.contains('_active')) {
+    //             openModalWindow();
+    //         }
+    //     }
 
-        if (modal.classList.contains('_active') && !modal.querySelector('.modal__body').contains(event.target) && !event.target.closest('[data-cross-button]')) {
-            closeModalWindow();
-        }
-    });
+    //     if (modal.classList.contains('_active') && !modal.querySelector('.modal__body').contains(event.target) && !event.target.closest('[data-cross-button]')) {
+    //         closeModalWindow();
+    //     }
+    // });
 
 
-    if (сloseBtn) {
-        сloseBtn.addEventListener('click', closeModalWindow);
-    }
+    // if (сloseBtn) {
+    //     сloseBtn.addEventListener('click', closeModalWindow);
+    // }
 
-    if (backToCatalogButton) {
-        backToCatalogButton.addEventListener('click', closeModalWindow);
-    }
+    // if (backToCatalogButton) {
+    //     backToCatalogButton.addEventListener('click', closeModalWindow);
+    // }
 
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'Escape' && modal.classList.contains('_active')) {
-            closeModalWindow();
-        }
-    });
+    // document.addEventListener('keydown', (event) => {
+    //     if (event.key === 'Escape' && modal.classList.contains('_active')) {
+    //         closeModalWindow();
+    //     }
+    // });
 }
